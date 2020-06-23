76f4bed096160bdd50e399e57ef8da84008bc383
Trish Ness <trishnss@gmail.com>
2020-06-23 16:24:43 -0700

    button bug fixed, UVI color index added, opening page function added

--- script.js
+++ script.js
@@ -5,54 +5,12 @@ var currentDate = moment().format("M/DD/YYYY");
 var cityArray = JSON.parse(localStorage.getItem("cityData")) || [];
 
 
-//NEED TO MAKE THE OPENING PAGE OPEN TO THE LAST SEARCHED ITEM
-//NEED TO DO THE UVI INDEX COLOR
-//NEED TO MAKE THE SEARCHED PAGE DISPLAY WHEN SEARCH BUTTON CLICKED
-
-
-//Loading page for Seattle so that the website doesn't open up broken... should be a way to fix this!!!
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response) {
-    var temp = response.list[0].main.temp;
-    var humidity = response.list[0].main.humidity;
-    var wind = response.list[3].wind.speed;
-    var lat = response.city.coord.lat;
-    var lon = response.city.coord.lon;
-    var icon = response.list[0].weather[0].icon;
-    var queryURLuvi = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&appid=2acfaadbcdceb6ac258c5d746dd4e2a2";
-    
-    //Incorporate Seattle UVI
-    $.ajax({
-        url: queryURLuvi,
-        method: "GET"
-    }).then(function (response) {
-        var uvi = response.current.uvi
-        $("div.uvi").text("UVI: " + uvi);
-    })
-
-    //Display current Seattle stats
-    $("div.city").html("<h2>" + response.city.name + "</h2>");
-    $("div.city").append("<h2>" + currentDate + "</h2>")
-    $("div.temp").text("Temperature: " + temp + " °F");
-    $("div.humidity").text("Humidity: " + humidity + "%");
-    $("div.wind").text("Wind: " + wind + " mph");
-    $("#icon").attr("src", "http://openweathermap.org/img/wn/" + icon + ".png")
-
-    //5 day forecast Seattle
-    for (i = 0; i < 6; i++) {
-        $("#day" + [i]).append((moment().add(i, 'days').format("M/" + "DD" + "/YYYY")));
-        var imgToAttach = response.list[i].weather[0].icon;
-        $("#day" + [i] + "Img").attr("src", "http://openweathermap.org/img/wn/" + imgToAttach + ".png")
-        $("#day" + [i] + "temp").append(response.list[i].main.temp + "°F");
-        $("#day" + [i] + "humid").text("Humidity: " + response.list[i].main.humidity + "%");
-    }
-});
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////
-//functions for click events and local storage, user interaction
-////////////////////////////////////////////////////////////////////////////////////////////////////
+//Populate page when opened
+if (cityArray.length > 0) {
+    renderData(cityArray[cityArray.length -1])
+} else {
+    renderData("Seattle")
+}
 
 //Search history sidebar
 renderSearchHistory();
@@ -82,7 +40,17 @@ function renderData(cityName) {
             method: "GET"
         }).then(function (response) {
             var uvi = response.current.uvi
-            $("div.uvi").text("UVI: " + uvi);
+            $("div.uvi").text("UV Index: " + uvi);
+            console.log(uvi)
+        
+        //UV Index Danger Level
+        if (uvi < 2) {
+            $("div.uvi").attr("style", "background-color:green; color:white")
+        } if (uvi >2 && uvi<5) {
+            $("div.uvi").attr("style", "background-color:yellow")
+        } if (uvi > 5) {
+            $("div.uvi").attr("style", "background-color:red; color:white")
+        }
         })
 
         //Display current stats
@@ -92,7 +60,7 @@ function renderData(cityName) {
         $("div.humidity").text("Humidity: " + humidity + "%");
         $("div.wind").text("Wind: " + wind + " mph");
 
-        //5 day forecast, why is this double printing?
+        //5 day forecast
         for (i = 0; i < 6; i++) {
             $("#day" + [i]).text((moment().add(i, 'days').format("M/" + "DD" + "/YYYY")));
             var imgToAttach = response.list[i].weather[0].icon;
@@ -129,8 +97,14 @@ $("#searchBtn").on("click", function (event) {
     event.preventDefault();
 
     var citySearch = $("#citySearch").val().trim();
+    if (cityArray.indexOf(citySearch) === -1) {
+        cityArray.push(citySearch)
+    }
+    if (cityArray.length > 5) {
+        cityArray.shift()
+    }
 
-    cityArray.push(citySearch);
     localStorage.setItem("cityData", JSON.stringify(cityArray))
     renderSearchHistory();
+    renderData(citySearch);
 })
